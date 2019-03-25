072e51c4974ca2c84f0c9eba16e68e9548fb95ee
Mr. X <dave.carter@gmail.com>
2019-03-25 09:25:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,8 @@ yum update -y
 yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 logout
 #we force a logout so that the user can assume permission over the docker service
-
-#this is for Ubunutu (apt)
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install docker
-sudo gpasswd -a $USER docker
-sudo apt-get install docker-compose
-docker-compose up -d
 
